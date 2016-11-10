533c81b348a2646d3851c06364dcace9a88b1ab7
Conner D <connerdunn7399@gmail.com>
2016-11-10 14:57:29 -0700

    Fixed the mixed indentation

--- voting-client/src/components/Results_spec.jsx
+++ voting-client/src/components/Results_spec.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-	renderIntoDocument,
-	scryRenderedDOMComponentsWithClass
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithClass
 } from 'react-addons-test-utils';
 import { List, Map } from 'immutable';
 import Results from '../../src/components/Results';
@@ -10,20 +10,20 @@ import {expect} from 'chai';
 
 describe('Results', () => {
 
-	it('renders entries with vote counts or zeros', () => {
+  it('renders entries with vote counts or zeros', () => {
     const pair = List.of('23', 'Star Wars');
     const tally = Map({'Star Wars': 111});
-		const component = renderIntoDocument(
-			<Results pair={pair} tally={tally} />
-		);
-		const entries = scryRenderedDOMComponentsWithClass(component, 'entry');
+    const component = renderIntoDocument(
+      <Results pair={pair} tally={tally} />
+    );
+    const entries = scryRenderedDOMComponentsWithClass(component, 'entry');
     const [two, star] = entries.map(e => e.textContent);
 
-		expect(entries.length).to.equal(2);
+    expect(entries.length).to.equal(2);
     expect(two).to.contain('23');
     expect(two).to.contain('0');
     expect(star).to.contain('Star Wars');
     expect(star).to.contain('111');
-	});
+  });
 
 });
