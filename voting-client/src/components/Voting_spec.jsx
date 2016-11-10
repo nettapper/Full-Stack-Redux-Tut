6f843d66e6e424fc28bd3070f640c36dadf4e944
Conner D <connerdunn7399@gmail.com>
2016-11-10 12:15:13 -0700

    should render the winner when winner is passed on

--- voting-client/src/components/Voting_spec.jsx
+++ voting-client/src/components/Voting_spec.jsx
@@ -60,4 +60,17 @@ describe('Voting', () => {
 		expect(buttons[0].textContent).to.contain('Voted');
 	});
 
+	it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner="23" />
+    );
+
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		expect(buttons.length).to.equal(0);
+
+    const winner = ReactDOM.findDOMNode(component, 'button');
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('23');
+	});
+
 });
