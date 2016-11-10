744894293982e3f3b073386151b39aa335568381
Conner D <connerdunn7399@gmail.com>
2016-11-10 13:23:22 -0700

    Testing that the PureRenderMixin is working

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Vote from './Vote';
 import Winner from './Winner';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>
       { this.props.winner ?
--- voting-client/src/components/Voting_spec.jsx
+++ voting-client/src/components/Voting_spec.jsx
@@ -5,6 +5,7 @@ import {
 	scryRenderedDOMComponentsWithTag,
 	Simulate
 } from 'react-addons-test-utils';
+import { List } from 'immutable';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
@@ -71,6 +72,26 @@ describe('Voting', () => {
     const winner = ReactDOM.findDOMNode(component, 'button');
     expect(winner).to.be.ok;
     expect(winner.textContent).to.contain('23');
+	});
+
+	it('renders as a pure component', () => {
+    const pair = ['23', 'Star Wars'];
+    const container = document.createElement('div');
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('23');
+
+    pair[0] = 'this shouldnt be the new text';
+    component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    );
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('23');
 	});
 
 });
