4940d4e963f4c9a789ef50284f1ad35dcdd0792a
Conner D <connerdunn7399@gmail.com>
2016-11-10 14:02:40 -0700

    Ok so the Results page is showing the results

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
-const pair = List.of('23', 'Star Wars');
+const pair  = List.of('23', 'Star Wars');
+const tally = Map({'23': 21, 'Star Wars': 111});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {pair: pair});
+    return React.cloneElement(this.props.children, {
+      pair:  pair,
+      tally: tally
+    });
   }
 });
